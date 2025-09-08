49dfcf19a60c27aa1ee9d3ca7bfa10393bd46d33
nadav elgrabli <nadav.elgrabli@live.biu.ac.il>
2025-09-08 09:43:36 +0300

    before splitting map into seperate module

--- frontend/src/app/components/map/map.module.ts
+++ frontend/src/app/components/map/map.module.ts
@@ -1,8 +1,7 @@
-// import { NgModule } from '@angular/core';
-// import { MapComponent } from './map.component';
+import { NgModule } from '@angular/core';
 
-// @NgModule({
-//   declarations: [MapComponent],
-//   exports: [MapComponent],
-// })
-// export class MapModule {}
+@NgModule({
+  declarations: [],
+  exports: [],
+})
+export class MapModule {}
