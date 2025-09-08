0531d20565029da9bd5744a786a2e2d5636224bb
nadav elgrabli <nadav.elgrabli@live.biu.ac.il>
2025-09-08 15:31:21 +0300

    added friends service

--- frontend/src/app/components/map/map-friends-list/map-friends-list.component.ts
+++ frontend/src/app/components/map/map-friends-list/map-friends-list.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../../../models/user.model';
 import { AuthService } from '../../../services/auth.service';
-import { HttpClient } from '@angular/common/http';
+import { FriendsService } from '../../../services/friends.service';
 
 @Component({
   selector: 'app-map-friends-list',
@@ -13,7 +13,10 @@ export class MapFriendsListComponent {
   friends: User[] = [];
   sortBy: 'name' | 'weight' | 'distance' = 'name';
 
-  constructor(private authService: AuthService, private http: HttpClient) {}
+  constructor(
+    private authService: AuthService,
+    private friendsService: FriendsService
+  ) {}
 
   ngOnInit() {
     this.authService.user.subscribe((user) => {
@@ -27,69 +30,28 @@ export class MapFriendsListComponent {
   loadFriends() {
     if (!this.loggedInUser) return;
 
-    this.http
-      .get<User[]>(
-        `http://localhost:5202/api/users/${this.loggedInUser.id}/friends`
-      )
+    this.friendsService
+      .getFriends(this.loggedInUser.id)
       .subscribe((friends) => {
-        this.friends = friends;
-        this.sortFriends();
+        this.friends = this.friendsService.sortFriends(
+          friends,
+          this.loggedInUser,
+          this.sortBy
+        );
       });
   }
 
   sortFriends() {
     if (!this.loggedInUser) return;
 
-    switch (this.sortBy) {
-      case 'name':
-        this.friends.sort((a, b) => a.username.localeCompare(b.username));
-        break;
-      case 'weight':
-        this.friends.sort((a, b) => Number(a.weight) - Number(b.weight));
-        break;
-      case 'distance':
-        const loggedLat = Number(this.loggedInUser.currentLocation.latitude);
-        const loggedLng = Number(this.loggedInUser.currentLocation.longitude);
-        this.friends.sort((a, b) => {
-          const distA = this.getDistance(
-            loggedLat,
-            loggedLng,
-            Number(a.currentLocation.latitude),
-            Number(a.currentLocation.longitude)
-          );
-          const distB = this.getDistance(
-            loggedLat,
-            loggedLng,
-            Number(b.currentLocation.latitude),
-            Number(b.currentLocation.longitude)
-          );
-          return distA - distB;
-        });
-        break;
-    }
+    this.friends = this.friendsService.sortFriends(
+      this.friends,
+      this.loggedInUser,
+      this.sortBy
+    );
   }
 
-  //not my code
-  public getDistance(
-    lat1: number,
-    lng1: number,
-    lat2: number,
-    lng2: number
-  ): number {
-    const R = 6371;
-    const dLat = this.deg2rad(lat2 - lat1);
-    const dLng = this.deg2rad(lng2 - lng1);
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(this.deg2rad(lat1)) *
-        Math.cos(this.deg2rad(lat2)) *
-        Math.sin(dLng / 2) *
-        Math.sin(dLng / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return R * c;
-  }
-
-  private deg2rad(deg: number): number {
-    return deg * (Math.PI / 180);
+  getDistance(lat1: number, lng1: number, lat2: number, lng2: number): number {
+    return this.friendsService.getDistance(lat1, lng1, lat2, lng2);
   }
 }
